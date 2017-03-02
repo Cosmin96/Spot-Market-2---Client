fafa78d11edb1743861188a564239c72c0b4e517
Cosmin Nicolae <marnicos2006@yahoo.com>
2017-03-02 17:51:38 +0000

    First version of app

---
+++ Spot Market/MainActivity.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Spot_Market
+{
+    [Activity(Label = "Spot_Market", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        
+        protected override void OnCreate(Bundle bundle)
+        {
+
+            base.OnCreate(bundle);
+            Button login = FindViewById<Button>(Resource.Id.button1); //Declare login button
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+            login.Click += delegate {
+                StartActivity(typeof(MainMenu)); //Load second activity
+            };
+        }
+    }
+}
+
